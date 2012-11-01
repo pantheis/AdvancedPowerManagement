42fa3565e5a271c082c7f89cdc14849bec3a977b
Cj Bowman <pantheis@kaijin.com>
2012-10-31 22:32:14 -0700

    Updated required IC2 and Forge versions in "@Mod" string. About time we did that!

--- src/common/com/kaijin/ChargingBench/ChargingBench.java
+++ src/common/com/kaijin/ChargingBench/ChargingBench.java
@@ -30,7 +30,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "ChargingBench", name="Charging Bench", version=CommonProxy.VERSION, dependencies = "required-after:IC2@[1.106,);required-after:Forge@[4.1.1.251,)")
+@Mod(modid = "ChargingBench", name="Charging Bench", version=CommonProxy.VERSION, dependencies = "required-after:IC2@[1.108,);required-after:Forge@[6.0.1.349,)")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false,
 clientPacketHandlerSpec = @SidedPacketHandler(channels = {"ChargingBench"}, packetHandler = ClientPacketHandler.class),
 serverPacketHandlerSpec = @SidedPacketHandler(channels = ("ChargingBench"), packetHandler = ServerPacketHandler.class))
