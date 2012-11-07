538cc80be59d9a01f2a79f880d36d4a9724726bd
Yancarlo Ramsey <yancarlo@gmail.com>
2012-11-07 15:23:43 -0800

    Average only recalculated when data changes
    Also data is now initialized with max delay so that on world load, low
    EU usage doesn't cause the reported average to spike far above the
    correct value and slowly settle. Instead it will gradually rise over
    time to the correct value.

--- src/common/com/kaijin/AdvPowerMan/MovingAverage.java
+++ src/common/com/kaijin/AdvPowerMan/MovingAverage.java
@@ -8,6 +8,9 @@ public class MovingAverage
 	//protected int packetTotal;
 	//protected int delayTotal;
 	protected int delay;
+	protected int window;
+	protected float average;
+	protected float delta;
 
 	public MovingAverage(int size)
 	{
@@ -17,16 +20,19 @@ public class MovingAverage
 		//packetTotal = 0;
 		//delayTotal = size;
 		delay = 1;
+		window = size;
+		average = 0F;
+		delta = 0F;
 		for (int i = 0; i < size; i++)
 		{
 			packets[i] = 0;
-			delays[i] = 1;
+			delays[i] = 600;
 		}
 	}
 
 	public void tick(int value)
 	{
-		if (value > 0 || delay > 600) // 600 ticks (30 sec) is long enough for 1 EU/t to have triggered a 512 EU packet by now
+		if (value > 0 || delay >= 600) // 600 ticks (30 sec) is long enough for 1 EU/t to have triggered a 512 EU packet by now
 		{
 			position++;
 			if (position >= packets.length) position = 0;
@@ -39,22 +45,38 @@ public class MovingAverage
 			delays[position] = delay;
 			//delayTotal += delay;
 			delay = 1;
+
+			window = sumDelays();
+			final float newAvg = ((float)sumPackets()) / ((float)window);
+			delta = newAvg - average;
+			average = newAvg;
 		}
 		else delay++;
 	}
 
-	public int getLength()
+	protected int sumDelays()
 	{
-		return packets == null ? 0 : packets.length;
+		if (delays == null) return 1;
+		int delayTotal = 0;
+		for (int d : delays) delayTotal += d;
+		return delayTotal;
+	}
+
+	protected int sumPackets()
+	{
+		if (packets == null) return 0;
+		int packetTotal = 0;
+		for (int p : packets) packetTotal += p;
+		return packetTotal;
 	}
 
 	public float getAverage()
 	{
-		if (packets == null) return 0;
-		int packetTotal = 0;
-		int delayTotal = 0;
-		for (int p : packets) packetTotal += p;
-		for (int d : delays) delayTotal += d;
-		return ((float)packetTotal) / ((float)delayTotal);
+		return average;
+	}
+
+	public int getWindow()
+	{
+		return window;
 	}
 }
